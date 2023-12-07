b52e8dfa3a036a3bbecf2bb572d884db76253d3d
NIKHILSharoff <149757007+NIKHILSharoff@users.noreply.github.com>
2023-12-07 21:09:48 +0530

    Update instance.tf
    availability zone updated

--- terraform-aws-rds-mariaDb/instance.tf
+++ terraform-aws-rds-mariaDb/instance.tf
@@ -11,14 +11,14 @@ resource "aws_instance" "rds_example" {
 
   # Security group assign to instance
   vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
-  availability_zone="us-east-1a"
+  availability_zone="ap-southeast-1a"
   # key name
   key_name = var.key_name
 
   user_data = <<EOF
     #! /bin/bash
-                sudo yum update -y
-    sudo yum install -y httpd.x86_64
+                sudo apt update -y
+    sudo apt install -y httpd.x86_64
     sudo service httpd start
     sudo service httpd enable
     echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
