b10adc352079d7a164390c2d5076c921cf7589c8
NIKHILSharoff <149757007+NIKHILSharoff@users.noreply.github.com>
2023-12-07 21:13:43 +0530

    Update ec2_jenkins.tf

--- EC2withjenkins/ec2_jenkins.tf
+++ EC2withjenkins/ec2_jenkins.tf
@@ -9,15 +9,15 @@
 
   user_data = <<EOF
                #! /bin/bash
-               sudo yum update
-               sudo yum install -y httpd.x86_64
+               sudo apt update
+               sudo apt install -y httpd.x86_64
                sudo service httpd start
                sudo service httpd enable
                echo "<h1>Deployed via Terraform<h1>" | sudo tee /var/www/html/inder.html 
-    yum install java-1.8.0-openjdk-devel -y
+    apt install java-1.8.0-openjdk-devel -y
     curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
     sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
-    yum install -y jenkins
+    apt install -y jenkins
     systemctl start jenkins
     systemctl status jenkins
     systemctl enable jenkins
