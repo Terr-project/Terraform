a4b5589b93b52372bba061d25057bbea3a5e055e
NIKHILSharoff <149757007+NIKHILSharoff@users.noreply.github.com>
2024-01-20 20:51:00 +0530

    Update vpc.tf

--- ebs-with-userdata/vpc.tf
+++ ebs-with-userdata/vpc.tf
@@ -21,7 +21,7 @@ resource "aws_subnet" "public_1" {
   availability_zone       = "ap-southeast-1a"
   vpc_id                  = aws_vpc.vpc_demo.id
   map_public_ip_on_launch = true
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.0.0.0/24"
 
   tags = {
     Name = "public_1-demo"
